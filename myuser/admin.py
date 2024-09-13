f5828c61db82c1d0df173cdfe6f3ff1d05a0cbe1
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2024-09-13 13:42:11 +0100

    Populated DB with seed data successfully

--- myuser/admin.py
+++ myuser/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from myuser.models import CustomUser
 # Register your models here.
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+  class Meta:
+    model = CustomUser
+    fields = '__all__'
--- myutils/seed.py
+++ myutils/seed.py
@@ -1,7 +1,69 @@
-import uuid
+from uuid import uuid4
+from django.contrib.auth import get_user_model
+from greenplan.models import Event, BulletinTemplate
 from datetime import datetime, timedelta
-from greenplan.models import BulletinTemplate
+from django.contrib.auth.hashers import make_password
 # Generate sample seed data
+
+Users = get_user_model()
+User = Users.objects.all()
+
+Events = Event.objects.all()
+Bulletin = BulletinTemplate.objects.all()
+
+
+user_seed_data = [
+    {
+        "id": uuid4(),
+        "email": "bayo.adesina@example.com",
+        "first_name": "Bayo",
+        "last_name": "Adesina",
+        "password": make_password("password123"),
+        "date_joined": datetime.now(),
+    },
+    {
+        "id": uuid4(),
+        "email": "amina.muhammad@example.com",
+        "first_name": "Amina",
+        "last_name": "Muhammad",
+        "password": make_password("password123"),
+        "date_joined": datetime.now(),
+    },
+    {
+        "id": uuid4(),
+        "email": "john.ekene@example.com",
+        "first_name": "John",
+        "last_name": "Ekene",
+        "password": make_password("password123"),
+        "date_joined": datetime.now(),
+    },
+    {
+        "id": uuid4(),
+        "email": "fatima.kane@example.com",
+        "first_name": "Fatima",
+        "last_name": "Kane",
+        "password": make_password("password123"),
+        "date_joined": datetime.now(),
+    },
+    {
+        "id": uuid4(),
+        "email": "kwame.owusu@example.com",
+        "first_name": "Kwame",
+        "last_name": "Owusu",
+        "password": make_password("password123"),
+        "date_joined": datetime.now(),
+    },
+    {
+        "id": uuid4(),
+        "email": "isaac@example.com",
+        "first_name": "Isaac",
+        "last_name": "Eli",
+        "password": make_password("password123"),
+        "date_joined": datetime.now(),
+    }
+
+]
+
 
 event_seed_data = [
     {
@@ -10,7 +72,7 @@ event_seed_data = [
         "location": "Nairobi, Kenya",
         "start_time": datetime.now() + timedelta(days=1),
         "end_time": datetime.now() + timedelta(days=1, hours=3),
-        "organizer": uuid.uuid4(),
+        "organizer": User.get(email=user_seed_data[0]['email']),
         "slug": "african-tech-summit-2024"
     },
     {
@@ -19,7 +81,7 @@ event_seed_data = [
         "location": "Johannesburg, South Africa",
         "start_time": datetime.now() + timedelta(days=30),
         "end_time": datetime.now() + timedelta(days=35),
-        "organizer": uuid.uuid4(),
+        "organizer": User.get(email=user_seed_data[2]['email']),
     },
     {
         "code": "AGRI2024",
@@ -27,7 +89,8 @@ event_seed_data = [
         "location": "Accra, Ghana",
         "start_time": datetime.now() + timedelta(days=20),
         "end_time": datetime.now() + timedelta(days=25),
-        "organizer": uuid.uuid4(),  # Assuming you have a User model with ID 3
+        # Assuming you have a User model with ID 3
+        "organizer": User.get(email=user_seed_data[1]['email']),
     },
     {
         "code": "HALE2024",
@@ -35,7 +98,7 @@ event_seed_data = [
         "location": "Lagos, Nigeria",
         "start_time": datetime.now(),
         "end_time": datetime.now() + timedelta(hours=4),
-        "organizer": uuid.uuid4(),
+        "organizer": User.get(email=user_seed_data[3]['email']),
         "slug": "african-academic-summit-2024"
     },
     {
@@ -44,7 +107,7 @@ event_seed_data = [
         "location": "Accra, Ghana",
         "start_time": datetime.now() + timedelta(days=1),
         "end_time": datetime.now() + timedelta(days=1, hours=3),
-        "organizer": uuid.uuid4(),
+        "organizer":User.get(email=user_seed_data[4]['email']),
         "slug": "african-fintech-conference-2024"
     },
     {
@@ -53,7 +116,7 @@ event_seed_data = [
         "location": "Nairobi, Kenya",
         "start_time": datetime.now() + timedelta(days=2),
         "end_time": datetime.now() + timedelta(days=2, hours=5),
-        "organizer": uuid.uuid4(),
+        "organizer": User.get(email=user_seed_data[4]['email']),
         "slug": "agricultural-innovations-summit"
     },
     {
@@ -62,7 +125,7 @@ event_seed_data = [
         "location": "Johannesburg, South Africa",
         "start_time": datetime.now() + timedelta(days=3),
         "end_time": datetime.now() + timedelta(days=3, hours=4),
-        "organizer": uuid.uuid4(),
+        "organizer": User.get(email=user_seed_data[3]['email']),
         "slug": "african-youth-forum-2024"
     },
     {
@@ -71,48 +134,52 @@ event_seed_data = [
         "location": "Cape Town, South Africa",
         "start_time": datetime.now() + timedelta(days=4),
         "end_time": datetime.now() + timedelta(days=4, hours=6),
-        "organizer": uuid.uuid4(),
+        "organizer":User.get(email=user_seed_data[2]['email']),
         "slug": "google-developers-summit-africa"
     }
 ]
 
 
 bulletin_template_seed_data = [
-  
-  {
+
+    {
         "code": "BUL001",
         "title": "Standard Event Bulletin",
+        "event_name": Events.get(code=event_seed_data[4]['code']),
         "description": "A general-purpose bulletin template for events.",
     },
     {
         "code": "BUL002",
         "title": "Academic Conference Bulletin",
+        "event_name": Events.get(code=event_seed_data[3]['code']),
+
         "description": "A template specifically designed for academic conferences.",
     },
     {
         "code": "BUL003",
         "title": "Financial Summit Bulletin",
+        "event_name": Events.get(code=event_seed_data[1]['code']),
+
         "description": "A template tailored for financial summits.",
     },
 
     {
         "code": "BULLETIN-ACADA",
-        "title": "Academic Summit Bulletin",
-        "event_name": 1,  # Assume this refers to the first event "African Academic Summit 2024"
-        "slug": "academic-summit-bulletin",
+       "title": "Academic Summit Bulletin",
+        "event_name": Events.get(code=event_seed_data[3]['code']),  # Assume this refers to the first event "African Academic Summit 2024"
+
         "description": "The official bulletin for African Academic Summit 2024."
     },
     {
         "code": "BULLETIN-FINTECH",
         "title": "Fintech Conference Bulletin",
-        "event_name": 2,  # "African Fintech Conference 2024"
-        "slug": "fintech-conference-bulletin",
+        "event_name":Events.get(code=event_seed_data[4]['code']) ,  # "African Fintech Conference 2024"
         "description": "The official bulletin for African Fintech Conference 2024."
     },
     {
         "code": "BULLETIN-AGRICON",
         "title": "Agricultural Summit Bulletin",
-        "event_name": 3,  # "Agricultural Innovations Summit"
+        "event_name":Events.get(code=event_seed_data[4]['code']),  # "Agricultural Innovations Summit"
         "slug": "agricultural-summit-bulletin",
         "description": "The official bulletin for Agricultural Innovations Summit."
     }
@@ -121,7 +188,7 @@ bulletin_template_seed_data = [
 custom_field_seed_data = [
     {
         # Assume this refers to the first bulletin "Academic Summit Bulletin"
-        "bulletin_template": 1,
+        "bulletin_template": Bulletin.get(title=bulletin_template_seed_data[1]['title']) ,
         "label": "Welcome Address",
         "content": "Welcome speech by the event host.",
         "start_time": "09:00",
@@ -129,7 +196,71 @@ custom_field_seed_data = [
     },
     {
         # Assume this refers to the second bulletin "Fintech Conference Bulletin"
-        "bulletin_template": 2,
+        "bulletin_template": Bulletin.get(title=bulletin_template_seed_data[2]['title']),
+        "label": "Keynote Address",
+        "content": "Keynote speech by industry expert.",
+        "start_time": "10:00",
+        "end_time": "10:45"
+    },
+     {
+        # Assume this refers to the first bulletin "Academic Summit Bulletin"
+        "bulletin_template": Bulletin.get(title=bulletin_template_seed_data[2]['title']) ,
+        "label": "Welcome Address",
+        "content": "Welcome speech by the event host.",
+        "start_time": "09:00",
+        "end_time": "09:30"
+    },
+    {
+        # Assume this refers to the second bulletin "Fintech Conference Bulletin"
+        "bulletin_template": Bulletin.get(title=bulletin_template_seed_data[1]['title']) ,
+        "label": "Keynote Address",
+        "content": "Keynote speech by industry expert.",
+        "start_time": "10:00",
+        "end_time": "10:45"
+    },
+     {
+        # Assume this refers to the first bulletin "Academic Summit Bulletin"
+        "bulletin_template": Bulletin.get(title=bulletin_template_seed_data[3]['title']) ,
+        "label": "Welcome Address",
+        "content": "Welcome speech by the event host.",
+        "start_time": "09:00",
+        "end_time": "09:30"
+    },
+    {
+        # Assume this refers to the second bulletin "Fintech Conference Bulletin"
+        "bulletin_template": Bulletin.get(title=bulletin_template_seed_data[3]['title']),
+        "label": "Keynote Address",
+        "content": "Keynote speech by industry expert.",
+        "start_time": "10:00",
+        "end_time": "10:45"
+    },
+     {
+        # Assume this refers to the first bulletin "Academic Summit Bulletin"
+        "bulletin_template":Bulletin.get(title=bulletin_template_seed_data[4]['title']),
+        "label": "Welcome Address",
+        "content": "Welcome speech by the event host.",
+        "start_time": "09:00",
+        "end_time": "09:30"
+    },
+    {
+        # Assume this refers to the second bulletin "Fintech Conference Bulletin"
+        "bulletin_template": Bulletin.get(title=bulletin_template_seed_data[4]['title']),
+        "label": "Keynote Address",
+        "content": "Keynote speech by industry expert.",
+        "start_time": "10:00",
+        "end_time": "10:45"
+    },
+     {
+        # Assume this refers to the first bulletin "Academic Summit Bulletin"
+        "bulletin_template":Bulletin.get(title=bulletin_template_seed_data[5]['title']),
+        "label": "Welcome Address",
+        "content": "Welcome speech by the event host.",
+        "start_time": "09:00",
+        "end_time": "09:30"
+    },
+    {
+        # Assume this refers to the second bulletin "Fintech Conference Bulletin"
+        "bulletin_template": Bulletin.get(title=bulletin_template_seed_data[5]['title']),
         "label": "Keynote Address",
         "content": "Keynote speech by industry expert.",
         "start_time": "10:00",
@@ -137,27 +268,26 @@ custom_field_seed_data = [
     },
     {
         # Assume this refers to the third bulletin "Agricultural Summit Bulletin"
-        "bulletin_template": 3,
+        "bulletin_template":Bulletin.get(title=bulletin_template_seed_data[5]['title']),
         "label": "Panel Discussion",
         "content": "Discussion on latest agricultural technologies.",
         "start_time": "11:00",
         "end_time": "12:00"
     },
     {
-        "bulletin_template": 4,
+        "bulletin_template":Bulletin.get(title=bulletin_template_seed_data[3]['title']),
         "label": "Keynote Speaker",
         "content": "Dr. Amina J. Mohammed",
     },
     {
-        "bulletin_template": 5,
+        "bulletin_template": Bulletin.get(title=bulletin_template_seed_data[5]['title']),
         "label": "Session Topics",
         "content": "Sustainable Development, Gender Equality, Technology in Education",
     },
     {
-        "bulletin_template":6,
+        "bulletin_template":Bulletin.get(title=bulletin_template_seed_data[2]['title']),
         "label": "Featured Speakers",
         "content": "Ngozi Okonjo-Iweala, Akinwumi Adesina",
     },
 ]
 
-# event_seed_data, bulletin_template_seed_data, custom_field_seed_data
