c353a4ca5dc5c312c1bab2e8426a448f6b1d711a
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-01-16 22:03:53 +0100

    refactor(image validator): increased the size of image that can be uploaded to 5mb max

--- greenplan/validators.py
+++ greenplan/validators.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_file_size(file):
-    max_size = 4
+    max_size = 5
 
     if file.size > max_size * 1024* 1024:
         raise ValidationError(
